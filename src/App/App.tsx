dbfd2f660c4e42a856f64a153e1e90c10078cd0a
Marnix Harderwijk <marnix.harderwijk@rocketscience.io>
2020-06-23 22:33:08 +0200

    add basic routing

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,13 +1,47 @@
 import React from 'react';
 import './App.scss';
 import CharacterList from './shared/redux/containers/CharacterList';
+import { Router, Switch, Route, Link } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
 
 const App: React.FC<Record<string, unknown>> = () => {
   return (
-    <>
-      <h1 className="text-2xl">The Force Awakens</h1>
-      <CharacterList />
-    </>
+    <Router history={history}>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
+
+      <hr />
+      {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+      <Switch>
+        <Route exact path="/">
+          <h1 className="text-2xl">The Force Awakens</h1>
+          <CharacterList />
+        </Route>
+        <Route path="/about">
+          <h1 className="text-2xl">About</h1>
+        </Route>
+        <Route path="/dashboard">
+          <h1 className="text-2xl">Dashboard</h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
